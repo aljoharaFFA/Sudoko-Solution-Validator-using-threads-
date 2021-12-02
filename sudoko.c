4d4f41dd26543fc3e1c6eb280455ba25baa6b62f
AlJohara AlFaisal <63267433+aljoharaFFA@users.noreply.github.com>
2021-12-03 02:54:22 +0300

    sudoku part 1
    The first part to my university project with the help of project partners.
    In this first part we are creating a 2d array to have the user input values to create a sudoku puzzle. 
    For the struct part (input) I will be discussing it in the 2nd part.

---
+++ sudoko.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#define NUM_THREADS 27 
+//why 27 threads? for each grid in the sudoko there will be a thread validating
+
+// strcutures are just objects (like classes in java)
+typedef struct {
+  int row;
+  int column;
+} input;// input is the name of our 
+
+
+int sudoku[9][9] = {0}; //a sudoko puzzle is 9 by 9 table. Also we are setting the 2D array to 0, so all enteries within the array is set to 0.
+
+int main() {
+  
+    int i,j; //global variable, so we dont have to type int everytime we want to implenet something, so just typing i or j quick is much simpler.
+  
+  //here we will be taking input from the user, the syntax:
+  printf("enter sodoku puzzle");// i is for row, j is for column
+  for(i = 0; i < 9; i++){
+    for(j = 0; j < 9; j++) {
+      scanf("%d", &sudoku[i][j]);
+    }
+  }
+  //priting sudoku
+  printf("\n your soduku is");
+  for(i = 0; i < 9; i++){
+    for (j = 0; j< 9; j++){
+      printf("%d", sudoku[i][j]);
+    }
+      printf("\n");
+  }
+  
+  //------------------------------------------------------------
+  
+  
+}
